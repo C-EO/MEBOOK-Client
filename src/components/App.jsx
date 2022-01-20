47a26e58314ff512f83b41ea3059abb6b6c31443
Moustapha Mahmoud <mmm066550@gmail.com>
2022-01-20 04:12:58 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,7 +22,7 @@ export default function App() {
     <BrowserRouter>
       <Temp />
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
