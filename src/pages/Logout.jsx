ed3cff2c578cddd7d65af3e3a02980e9a5ebaaed
Moustapha Mahmoud <mmm066550@gmail.com>
2022-01-24 19:49:50 +0200

    ...

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -2,13 +2,17 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { withCookies } from "react-cookie";
+import { logoutUser } from "../redux/actions";
 
 const mapStateToProps = (state) => state;
 export default withCookies(
-  connect(mapStateToProps)(function Login({ cookies }) {
+  connect(mapStateToProps, { logoutUser })(function Login({
+    cookies,
+    logoutUser,
+  }) {
     const navigate = useNavigate();
     useEffect(() => {
-      cookies.remove("jwt");
+      logoutUser();
       cookies.remove("user");
       navigate("/");
     }, []);
