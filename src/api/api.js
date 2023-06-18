b8a2f4e2524731261db0926f0f3bed367ab453a9
Moustapha Mahmoud <mmm066550@gmail.com>
2023-06-18 19:21:52 +0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 export default axios.create({
   // baseURL: "http://localhost:4040/api",
-  baseURL: "https://mebook-server.herokuapp.com/api",
+  baseURL: "https://mebook-production.up.railway.app/api",
 });
