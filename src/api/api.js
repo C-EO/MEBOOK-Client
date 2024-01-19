a6036ea61b83ea6e080361e09f6cd5f79295f39e
Moustapha Mahmoud <mmm066550@gmail.com>
2024-01-19 14:52:07 +0200

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 export default axios.create({
   // baseURL: "http://localhost:4040/api",
-  baseURL: "mebook.up.railway.app/api",
+  baseURL: "https://mebook.up.railway.app/api",
 });
